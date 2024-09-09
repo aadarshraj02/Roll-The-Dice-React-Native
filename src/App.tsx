2498e5beca5113cbc806a38d129f9a309a3713ce
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-09 09:59:26 +0530

    imported all the dice images

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import DiceOne from '../public/dices/dice_1.png';
+import DiceTwo from '../public/dices/dice_2.png';
+import DiceThree from '../public/dices/dice_3.png';
+import DiceFour from '../public/dices/dice_4.png';
+import DiceFive from '../public/dices/dice_5.png';
+import DiceSix from '../public/dices/dice_6.png';
 
 const App = (): JSX.Element => {
   return (
