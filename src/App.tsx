b9b7f9977a8dd56478c55b94490d79fe40a4b416
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-09 10:05:38 +0530

    added DiceProps and Passed in dice to accept Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Image, ImageSourcePropType, StyleSheet, Text, View} from 'react-native';
+import React, {PropsWithChildren} from 'react';
 import DiceOne from '../public/dices/dice_1.png';
 import DiceTwo from '../public/dices/dice_2.png';
 import DiceThree from '../public/dices/dice_3.png';
 import DiceFour from '../public/dices/dice_4.png';
 import DiceFive from '../public/dices/dice_5.png';
 import DiceSix from '../public/dices/dice_6.png';
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType;
+}>;
+
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  );
+};
 
 const App = (): JSX.Element => {
   return (
